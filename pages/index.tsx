3e11b1bd689bd3bc74e3872f589f02c13ba38acb
Spencer Sams <spencers@gmail.com>
2025-06-30 13:45:00 -0700

    Add files via upload

--- pages/index.tsx
+++ pages/index.tsx
@@ -347,6 +347,12 @@ const Home: React.FC<HomeProps> = ({
             Trump-owned properties.
           </p>
 
+          <p>
+                Wondering why these trips cost so much? We've put together a detailed guide on the factors that influence the total cost.
+                <br/>
+                <Link href="/cost-breakdown"><a className={styles.costInfoLink}>View the Cost Breakdown</a></Link>
+          </p>
+
           <div className={styles.sourceButtonContainer}>
             <button
               className={styles.sourceButton}
@@ -415,14 +421,6 @@ const Home: React.FC<HomeProps> = ({
               </div>
             )}
           </div>
-        </div>
-
-        <div className={styles.blurb}>
-            <p>
-                Wondering why these trips cost so much? We've put together a detailed guide on the factors that influence the total cost.
-                <br/>
-                <Link href="/cost-breakdown"><a className={styles.costInfoLink}>View the Cost Breakdown</a></Link>
-            </p>
         </div>
 
         <div className={styles.legend}>
