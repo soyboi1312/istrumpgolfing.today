05ff273b04b7a92c9c6e549e994140a5e826271e
Spencer <spencers@gmail.com>
2025-11-27 14:06:23 -0800

    updated scripts

--- scripts/sync-golf-dates.ts
+++ scripts/sync-golf-dates.ts
@@ -122,7 +122,7 @@ async function main() {
       const newEntries = newDates.map(({ date, location }) => `
         '${date}': {
             location: GolfLocation.${getLocationEnumKey(location)},
-            url: 'https://factba.se/biden/calendar',
+            url: 'url: 'https://rollcall.com/factbase/trump/topic/calendar',
             type: 'golf',
         },`).join('');
 
